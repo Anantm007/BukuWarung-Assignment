1ec09b2cd98b3bba9965c9137d065e786d478867
Anant <anant.mathur007@gmail.com>
2021-04-09 17:11:43 +0530

    production ready

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -3,8 +3,10 @@ const BASE_URL = evalURL();
 function evalURL() {
   if (window.location.href.includes("192.168.0")) {
     return "http://192.168.0.108:5055";
-  } else {
+  } else if (window.location.href.includes("localhost")) {
     return "http://localhost:5055";
+  } else {
+    return "http://18.224.165.248:5055";
   }
 }
 
